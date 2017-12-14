3d6040365d731b8a9b51fdec7bb73911eeb73854
matentzn <nicolas.matentzoglu@gmail.com>
2017-12-14 14:11:08 +0000

    linux scripts

---
+++ linux/run_pipeline.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+pipeline=$1
+url=$2
+ontologyfile=$3
+outdir=$4
+overwrite=$5
+timeout=$6
+
+harvestpipeline=$pipeline"harvest/"
+harvestpipelineurl=$harvestpipeline"url/"
+harvestpipelinefile=$harvestpipeline"file/"
+
+
+# Collect the data: Execute all jars in harvest URL directory (this is necessary to be done first, to obtain the downloaded ontology)
+for file in `ls -p "$harvestpipelineurl"`; 
+do
+	jar=$harvestpipelineurl$file
+	echo $jar
+	java -Xms2G -Xmx12G -DentityExpansionLimit=100000000 -jar $jar $url $ontologyfile $outdir $overwrite $timeout
+done
+
+# Collect the data: Execute all jars in harvest FILE directory
+ 
+for file in `ls -p "$harvestpipelinefile"`; 
+do
+	jar=$harvestpipelinefile$file
+	echo $jar
+	java -Xms2G -Xmx12G -DentityExpansionLimit=100000000 -jar $jar $url $ontologyfile $outdir $overwrite $timeout
+done
