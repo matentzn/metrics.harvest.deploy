d6f951b049c17e013b11c13ca5347d12952be97b
matentzn <nicolas.matentzoglu@gmail.com>
2017-12-14 14:10:46 +0000

    Added Groups to harvester

--- win/run_pipeline.bat
+++ win/run_pipeline.bat
@@ -2,8 +2,9 @@ SET pipeline=%1
 SET url=%2
 SET file=%3
 SET outdir=%4
-SET overwrite=%5
-SET timeout=%6
+SET group=%5
+SET overwrite=%6
+SET timeout=%7
 
 SET harvestpipeline=%pipeline%harvest\
 SET harvestpipelineurl=%harvestpipeline%url
@@ -11,10 +12,10 @@ SET harvestpipelinefile=%harvestpipeline%file
 
 :: Collect the data: Execute all jars in harvest URL directory
 cd %harvestpipelineurl%
-for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %file% %outdir% %overwrite% %timeout%
+for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %group% %file% %outdir% %overwrite% %timeout%
 
 :: Collect the data: Execute all jars in harvest FILE directory
 cd %harvestpipelinefile%
-for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %file% %outdir% %overwrite% %timeout%
+for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %group% %file% %outdir% %overwrite% %timeout%
 
 cd %pipeline%
