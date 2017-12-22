fa62f910c1fe4120f09ed5bf1204215140360154
matentzn <nicolas.matentzoglu@gmail.com>
2017-12-22 11:27:07 +0000

    New version of harvest tools, gathering load time and more exception metrics

--- win/run_pipeline.bat
+++ win/run_pipeline.bat
@@ -1,6 +1,6 @@
 SET pipeline=%1
 SET url=%2
-SET file=%3
+SET ontologies=%3
 SET outdir=%4
 SET group=%5
 SET overwrite=%6
@@ -12,10 +12,10 @@ SET harvestpipelinefile=%harvestpipeline%file
 
 :: Collect the data: Execute all jars in harvest URL directory
 cd %harvestpipelineurl%
-for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %group% %file% %outdir% %overwrite% %timeout%
+for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %ontologies% %group% %outdir% %overwrite% %timeout%
 
 :: Collect the data: Execute all jars in harvest FILE directory
 cd %harvestpipelinefile%
-for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %group% %file% %outdir% %overwrite% %timeout%
+for /r %%i in (*.jar) do java -Xms2G -Xmx10G -jar %%i %url% %ontologies% %group% %outdir% %overwrite% %timeout%
 
 cd %pipeline%
